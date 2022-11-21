b71b109da5427c391a0011925770a08d2b782fdb
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-21 21:31:56 +0300

    add cube view

---
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/View/CubeView.cs
@@ -0,0 +1,26 @@
+using Com.Afb.RunGame.Presentation.Interactor;
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.RunGame.Presentation.View {
+    public class CubeView : MonoBehaviour, IPoolable<Transform> {
+        // Dependencies
+        [Inject]
+        private ICubeInteractor cubeInteractor;
+
+        // TODO remove after test
+        private void Start() {
+            cubeInteractor.InitializeCube();
+        }
+
+        // Public Methods
+        public void OnSpawned(Transform parent) {
+            transform.SetParent(parent);
+            cubeInteractor.InitializeCube();
+        }
+
+        public void OnDespawned() {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/View/MeshView.cs
@@ -0,0 +1,51 @@
+using Com.Afb.RunGame.Business.Model;
+using Com.Afb.RunGame.Presentation.Presenter;
+using Com.Afb.RunGame.Presentation.View.Util;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.RunGame.Presentation.View {
+    public class MeshView : MonoBehaviour {
+        // Dependencies
+        [Inject]
+        private ICubeMeshPresenter cubeMeshPresenter;
+
+        // Private Properties
+        private MeshRenderer meshRenderer;
+        private MeshFilter meshFilter;
+
+        // Unity Methods
+        private void Awake() {
+            meshRenderer = GetComponent<MeshRenderer>();
+            meshFilter = GetComponent<MeshFilter>();
+
+            cubeMeshPresenter.Size
+                .TakeUntilDestroy(gameObject)
+                .Subscribe(OnSizeChange);
+
+            cubeMeshPresenter.Color
+              .TakeUntilDestroy(gameObject)
+              .Subscribe(OnColorChange);
+
+            cubeMeshPresenter.CubeCut
+                 .TakeUntilDestroy(gameObject)
+                 .Subscribe(OnCubeCut);
+        }
+
+        // Private Methods
+        private void OnColorChange(Color obj) {
+            meshRenderer.material.color = obj;
+        }
+
+        private void OnSizeChange(Vector3 size) {
+            var mesh = meshFilter.mesh;
+            CubeCreator.GenerateCube(mesh, size);
+            var collider = GetComponent<BoxCollider>();
+            collider.size = size;
+        }
+
+        private void OnCubeCut(CubeCutModel cut) {
+        }
+    }
+}
