a9dc8100bc838ab6aa0a8ed4a1340f256a312e0f
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-21 21:32:20 +0300

    add constants

---
+++ Assets/Scripts/Com/Afb/RunGame/Util/Constants.cs
@@ -0,0 +1,7 @@
+namespace Com.Afb.RunGame.Util {
+    public static class Constants {
+        public const float CUBE_HEIGHT = 0.4f;
+        public const float CUBE_WIDTH = 2.7f;
+        public const float CUBE_LENGTH = 2.7f;
+    }
+}
