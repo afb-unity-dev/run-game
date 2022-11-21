fcb38db44bec4fc128743a1e7f4eb12275287d80
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-21 21:32:30 +0300

    add installer

---
+++ Assets/Scripts/Com/Afb/RunGame/Installer/Game/BusinessInstaller.cs
@@ -0,0 +1,17 @@
+using Com.Afb.RunGame.Business.UseCase;
+using Zenject;
+
+namespace Com.Afb.RunGame.Installer.Game {
+    public class BusinessInstaller : MonoInstaller {
+        // Public Methods
+        public override void InstallBindings() {
+            BindGameUseCase();
+        }
+
+        // Private Methods
+        private void BindGameUseCase() {
+            Container.BindInterfacesTo<GameUseCase>()
+                .AsSingle();
+        }
+    }
+}
