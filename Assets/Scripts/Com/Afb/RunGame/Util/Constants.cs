88bac47ef261320b6bf07d1c24eb3a0728478a6b
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-23 10:07:22 +0300

    update contants

--- Assets/Scripts/Com/Afb/RunGame/Util/Constants.cs
+++ Assets/Scripts/Com/Afb/RunGame/Util/Constants.cs
@@ -1,11 +1,12 @@
 namespace Com.Afb.RunGame.Util {
     public static class Constants {
         public const float INITIAL_WIDTH = 7.475f;
+        public const float INITIAL_POSITION = 0.9f;
         public const float CUBE_HEIGHT = 0.4f;
         public const float CUBE_WIDTH = 2.7f;
         public const float CUBE_LENGTH = 2.7f;
-        public const float POSITION_THRESHOLD = 0.2f;
-        public const float LEFT_BOUNDARY = -INITIAL_WIDTH / 2 - CUBE_WIDTH;
-        public const float RIGHT_BOUNDARY = INITIAL_WIDTH / 2 + CUBE_WIDTH;
+        public const float POSITION_THRESHOLD = 0.3f;
+        public const float LEFT_BOUNDARY = -INITIAL_WIDTH / 2;
+        public const float RIGHT_BOUNDARY = INITIAL_WIDTH / 2;
     }
 }
