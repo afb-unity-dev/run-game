4d82f01b1b4b43fc09a554e2612e5b3323fbb0c0
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-24 01:06:20 +0300

    fix cute cube size zero position

--- Assets/Scripts/Com/Afb/RunGame/Business/Util/CutCube.cs
+++ Assets/Scripts/Com/Afb/RunGame/Business/Util/CutCube.cs
@@ -13,18 +13,24 @@ namespace Com.Afb.RunGame.Business.Util {
             newSize.x = newWidth;
 
             cube.Size = newSize;
-            cube.XPosition = left + newWidth / 2;
 
             var cubeCutModel = new CubeCutModel();
             cubeCutModel.Size = new Vector3(cutWidth, cube.Size.y, cube.Size.z);
             cubeCutModel.Color = cube.Color;
             cubeCutModel.Direction = cutDirection;
 
-            if (cutDirection <= Constants.CUT_DIRECTION_RIGHT) {
-                cubeCutModel.XPosition = right + cutWidth / 2;
+            if (cube.Size.x <= 0) {
+                cubeCutModel.XPosition = cube.XPosition;
             }
             else {
-                cubeCutModel.XPosition = left - cutWidth / 2;
+                cube.XPosition = left + newWidth / 2;
+
+                if (cutDirection <= Constants.CUT_DIRECTION_RIGHT) {
+                    cubeCutModel.XPosition = right + cutWidth / 2;
+                }
+                else {
+                    cubeCutModel.XPosition = left - cutWidth / 2;
+                }
             }
 
             return cubeCutModel;
