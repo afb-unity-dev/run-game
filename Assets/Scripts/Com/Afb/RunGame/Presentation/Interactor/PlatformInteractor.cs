f2ef5e1cb64b8b1dcb906c666e1ddfa4f6a31284
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-23 10:01:22 +0300

    add platform interactor

---
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/Interactor/PlatformInteractor.cs
@@ -0,0 +1,29 @@
+using Com.Afb.RunGame.Business.UseCase;
+using Com.Afb.RunGame.Presentation.Presenter;
+using UniRx;
+
+namespace Com.Afb.RunGame.Presentation.Interactor {
+    public class PlatformInteractor : IPlatformInteractor {
+
+        // Private Properties
+        private IGameUseCase gameUseCase;
+        private IPlatformPresenter platformPresenter;
+
+        // Constructor
+        public PlatformInteractor(IGameUseCase gameUseCase, IPlatformPresenter platformPresenter) {
+            this.gameUseCase = gameUseCase;
+            this.platformPresenter = platformPresenter;
+
+            gameUseCase.CharacterPosition.Subscribe(OnCharacterPositionChange);
+        }
+
+        private void OnCharacterPositionChange(int position) {
+            platformPresenter.SetCharacterPosition(position);
+        }
+
+        // Public Methods
+        public void PlaceCube(float xPosition) {
+            gameUseCase.PlaceCube(xPosition);
+        }
+    }
+}
