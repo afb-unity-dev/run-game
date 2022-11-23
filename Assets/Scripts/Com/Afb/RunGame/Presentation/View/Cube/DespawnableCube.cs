d598ccd19b7ec2b4bfeb4238aec5cb7a7f19b360
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-23 13:02:40 +0300

    add cube desapner

---
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/View/Cube/DespawnableCube.cs
@@ -0,0 +1,22 @@
+using Com.Afb.RunGame.Presentation.View.Util;
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.RunGame.Presentation.View {
+    public class DespawnableCube : MonoBehaviour {
+        // Serialize Fields
+        [SerializeField]
+        private CubeView cubeView;
+
+        // Dependencies
+        [Inject]
+        private MonoPoolableMemoryPool<Transform, Vector3, CubeView> cubeViewPool;
+
+        // Unity Functions
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.tag == ViewConstants.DESPAWNER_TAG) {
+                cubeViewPool.Despawn(cubeView);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/View/Platform/CubeSpawner.cs
@@ -0,0 +1,21 @@
+using Com.Afb.RunGame.Util;
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.RunGame.Presentation.View {
+    public class CubeSpawner : MonoBehaviour {
+        // Serialize Fields
+        [SerializeField]
+        private Transform cubeParent;
+
+        // Dependencies
+        [Inject]
+        private MonoPoolableMemoryPool<Transform, Vector3, CubeView> cubeViewPool;
+
+        // Public Methods
+        public CubeView Spawn(float zPosition) {
+            var position = new Vector3(0, -Constants.CUBE_HEIGHT / 2, zPosition);
+            return cubeViewPool.Spawn(cubeParent, position);
+        }
+    }
+}
