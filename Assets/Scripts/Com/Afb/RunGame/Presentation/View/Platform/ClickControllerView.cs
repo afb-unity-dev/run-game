8983367292e379f7300a568bc8d61de9a9edcae4
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-23 18:43:43 +0300

    fix click controller when click over ui

--- Assets/Scripts/Com/Afb/RunGame/Presentation/View/Platform/ClickControllerView.cs
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/View/Platform/ClickControllerView.cs
@@ -1,3 +1,6 @@
+using System;
+using Com.Afb.RunGame.Business.Util;
+using Com.Afb.RunGame.Presentation.Presenter;
 using Com.Afb.RunGame.Presentation.View.Util.InputHelper;
 using UniRx;
 using UnityEngine;
@@ -12,11 +15,25 @@ namespace Com.Afb.RunGame.Presentation.View {
         // Dependencies
         [Inject]
         private IClickEvent clickEvent;
+        [Inject]
+        private IGamePresenter gamePresenter;
 
+        private bool isPlaying;
 
         // Unity Methods
         private void Awake() {
+            gamePresenter.GameState
+                .TakeUntilDestroy(gameObject)
+                .Subscribe(OnGameStateChange);
+
             clickEvent.Click
+                .Where(input => {
+                    bool isOverUI = false;
+                    if (input.state == InputState.Begin) {
+                        isOverUI = Pointer.IsPointerOverUIObject(input.position);
+                    }
+                    return !isOverUI && isPlaying;
+                })
                 .TakeUntilDestroy(gameObject)
                 .Subscribe(OnClick);
         }
@@ -24,6 +41,26 @@ namespace Com.Afb.RunGame.Presentation.View {
         // Private Methods
         private void OnClick(InputData input) {
             platformView.OnClick();
+        }
+
+        private void OnGameStateChange(GameSate gameSate) {
+            if (gameSate == GameSate.Playing) {
+                /*
+                 * Hack: When a close button is cliked and popup closes, 
+                 * Pointer.IsPointerOverUIObject returns false.
+                 * So we dalay input and discard this click.
+                 */
+                Observable.Empty<object>()
+                    .DelayFrame(1)
+                    .DoOnCompleted(() => {
+                        isPlaying = true;
+                    })
+                    .Subscribe();
+              
+            }
+            else {
+                isPlaying = false;
+            }
         }
     }
 }
