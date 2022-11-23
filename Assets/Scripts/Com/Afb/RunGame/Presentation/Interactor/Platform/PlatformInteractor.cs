55ef6224c309ee532bb2310694aaa06bb36c5adc
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-23 13:49:33 +0300

    seperate platform interactor

--- Assets/Scripts/Com/Afb/RunGame/Presentation/Interactor/Platform/PlatformInteractor.cs
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/Interactor/Platform/PlatformInteractor.cs
@@ -1,34 +1,49 @@
+using System;
 using Com.Afb.RunGame.Business.UseCase;
 using Com.Afb.RunGame.Presentation.Presenter;
 using UniRx;
 
 namespace Com.Afb.RunGame.Presentation.Interactor {
-    public class PlatformInteractor : IPlatformInteractor {
+    public class PlatformInteractor : IPlatformInteractor, IPlatformCubeInteractor {
 
         // Readonly Properties
         private readonly ICubePlacementUseCase cubePlacementUseCase;
         private readonly IPlatformUseCase platformUseCase;
-        private readonly IPlatformPresenter platformPresenter;
+        private readonly IPlatformUpdatablePresenter platformUpdatablePresenter;
+        private readonly ICubeCreateUseCase cubeCreateUseCase;
 
         // Constructor
         public PlatformInteractor(ICubePlacementUseCase cubePlacementUseCase,
                 IPlatformUseCase platformUseCase,
-                IPlatformPresenter platformPresenter) {
+                IPlatformUpdatablePresenter platformUpdatablePresenter,
+                ICubeCreateUseCase cubeCreateUseCase) {
 
             this.platformUseCase = platformUseCase;
             this.cubePlacementUseCase = cubePlacementUseCase;
-            this.platformPresenter = platformPresenter;
+            this.platformUpdatablePresenter = platformUpdatablePresenter;
+            this.cubeCreateUseCase = cubeCreateUseCase;
 
             platformUseCase.CharacterPosition.Subscribe(OnCharacterPositionChange);
-        }
-
-        private void OnCharacterPositionChange(int position) {
-            platformPresenter.SetCharacterPosition(position);
+            platformUseCase.TargetPosition.Subscribe(OnTargetPositionChange);
         }
 
         // Public Methods
         public void PlaceCube(float xPosition) {
             cubePlacementUseCase.PlaceCube(xPosition);
         }
+
+        public void AddCube() {
+            cubeCreateUseCase.CreateNewCube();
+        }
+
+        // Private Methods
+        private void OnTargetPositionChange(int position) {
+            platformUpdatablePresenter.SetTargetPosition(position);
+        }
+
+        private void OnCharacterPositionChange(int position) {
+            platformUpdatablePresenter.SetCharacterPosition(position);
+        }
+
     }
 }
