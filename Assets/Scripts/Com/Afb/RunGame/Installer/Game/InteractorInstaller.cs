3ebfe6b645c4c8c00c4c71d42fae4d33b2c35005
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-23 10:02:01 +0300

    impletmenet platform installers

--- Assets/Scripts/Com/Afb/RunGame/Installer/Game/InteractorInstaller.cs
+++ Assets/Scripts/Com/Afb/RunGame/Installer/Game/InteractorInstaller.cs
@@ -1,12 +1,19 @@
+using Com.Afb.RunGame.Presentation.Interactor;
 using Zenject;
 
 namespace Com.Afb.RunGame.Installers.Game {
     public class InteractorInstaller : Installer {
         // Public Methods
         public override void InstallBindings() {
+            BindPlatformInteractor();
         }
 
         // Private Methods
+        private void BindPlatformInteractor() {
+            Container.BindInterfacesTo<PlatformInteractor>()
+                .AsSingle()
+                .Lazy();
+        }
     }
 }
 
