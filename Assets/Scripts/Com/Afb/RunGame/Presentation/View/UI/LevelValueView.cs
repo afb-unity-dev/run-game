95f63a9dd8b1d7af92c8aa869d3cb0116ca6a14d
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2022-11-24 03:53:40 +0300

    add level value view

---
+++ Assets/Scripts/Com/Afb/RunGame/Presentation/View/UI/LevelValueView.cs
@@ -0,0 +1,30 @@
+using Com.Afb.RunGame.Presentation.Presenter;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Com.Afb.RunGame.Presentation.View {
+    public class LevelValueView : MonoBehaviour {
+        // Private Properties
+        private TMP_Text levelText;
+
+        // Dependencies
+        [Inject]
+        private ILevelPresenter levelPresenter;
+
+        // Unity Methods
+        private void Awake() {
+            levelText = GetComponent<TMP_Text>();
+
+            levelPresenter.Level
+                .TakeUntilDestroy(gameObject)
+                .Subscribe(OnLevelChange);
+        }
+
+        // Private Properties
+        private void OnLevelChange(int level) {
+            levelText.text = level.ToString();
+        }
+    }
+}
